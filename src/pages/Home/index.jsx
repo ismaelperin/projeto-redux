b8ae18b6e466c111519c2dc08afa678e263fad79
ismaelperin <ismaelperin@hotmail.com>
2026-03-12 08:30:36 -0300

    Projeto Final redux

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,33 @@ import styles from './home.module.css'
 import { Header } from '../../components/header'
 import { Link } from 'react-router-dom'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteAddress, fetchUsers, fetchUserById } from '../../redux/user/slice'
+
 export function Home() {
+  const { user, users, loading } = useSelector((rootReducer) => rootReducer.user)
+  const dispatch = useDispatch();
+
 
   function handleDeleteAddress(){
+    dispatch(deleteAddress())
     alert("Endereço deletado com sucesso!")
+  }
+
+  function handleFetchUsers(){
+    dispatch(fetchUsers())
+  }
+
+  function handleFetchUserById(){
+    const userId = 8;
+
+    dispatch(fetchUserById(userId))
   }
 
   return (
     <>
     <Header/>
+    
       <div className={styles.container}>
         <nav className={styles.nav}>
           <Link to="/" className={styles.link}>
@@ -27,18 +45,41 @@ export function Home() {
         <main className={styles.content}>
           <div className={styles.message}>
             <h1 className={styles.title}>
-              Olá Visitante, bem vindo!
+              Olá {user ? user.name : "Visitante"}, bem vindo!
             </h1>
 
-            <span>Email: ....</span>
+            { user && (
+              <span>Email: {user.email}</span>
+            )}
 
 
-            <strong className={styles.addressLabel}>Endereço atual:</strong>
-            <div className={styles.address}>
-              <p>Rua centro, n 123</p>
-              
-              <button onClick={handleDeleteAddress}>Deletar endereço</button>
-            </div>
+            { user && user.address && (
+              <>
+                <strong className={styles.addressLabel}>Endereço atual:</strong>
+                <div className={styles.address}>
+                  <p>{user.address.location}, n {user.address.number}</p>
+                  
+                  <button onClick={handleDeleteAddress}>Deletar endereço</button>
+                </div>
+              </>
+            )}
+
+            <hr/>
+            <br/>
+
+            <h2>Lista de usuários</h2>
+            <button onClick={handleFetchUsers}>Buscar usuários</button>
+            <button onClick={handleFetchUserById}>Buscar usuário com ID</button>
+            <br/>
+
+
+            {loading && <strong>Carregando usuários...</strong> }
+
+            {!loading && users.map( (user) => (
+              <div key={user.id}>
+                <p>ID: {user.id} | {user.name}</p>
+              </div>
+            ))}
 
           </div>
 
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './root-reducer'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+})
+
+sagaMiddleware.run(rootSaga)
